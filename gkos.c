327524d04fc2dcda08c1791f972b598ca1e02c7e
Devin J. Pohly <djpohly@gmail.com>
2014-11-04 14:39:53 -0700

    split out gkos.h
    it's getting to the point where that makes sense

--- gkos.c
+++ gkos.c
@@ -6,13 +6,7 @@
 #include <X11/Xutil.h>
 #include <X11/extensions/XInput2.h>
 
-#define GRID_X 130
-#define GRID_Y 70
-#define TOP_Y 400
-
-#define PRESSED_COLOR 0xd0888a85
-#define UNPRESSED_COLOR 0xd0204a87
-#define BORDER_COLOR 0xffeeeeec
+#include "gkos.h"
 
 /*
  * 1 2 4 8 16 32
@@ -50,61 +44,6 @@
  * 63 - Numbers (lowercase->numbers->lowercase)
  */
 
-
-/*
- * Represents one button in a given layout
- */
-struct layout_btn {
-	int8_t x, y;
-	uint8_t h;
-	uint8_t bits;
-};
-
-/*
- * Window corresponding to button
- */
-struct layout_win {
-	Window win;
-	uint8_t bits;
-};
-
-/*
- * Default button layout (modeled after Android GKOS app)
- */
-static const struct layout_btn default_btns[] = {
-	{0, 0, 1, 1},
-	{0, 1, 1, 3},
-	{0, 2, 1, 2},
-	{0, 3, 1, 6},
-	{0, 4, 1, 4},
-
-	{1, 1, 1, 5},
-	{1, 2, 3, 7},
-
-	{-2, 1, 1, 40},
-	{-2, 2, 3, 56},
-
-	{-1, 0, 1, 8},
-	{-1, 1, 1, 24},
-	{-1, 2, 1, 16},
-	{-1, 3, 1, 48},
-	{-1, 4, 1, 32},
-};
-
-struct kbd_state {
-	Display *dpy;
-	XVisualInfo xvi;
-	Colormap cmap;
-	Window win;
-	GC gc;
-	int xi_opcode;
-	int input_dev;
-	int nwins;
-	int ntouches;
-	struct layout_win *wins;
-	struct layout_win **touches;
-	unsigned int active : 1;
-};
 
 
 /*
