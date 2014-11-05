7ad3613380c3abb2987a9d86deedc02fd81be60f
Devin J. Pohly <djpohly@gmail.com>
2014-11-05 16:11:53 -0700

    add chorder test

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-BINS = gkos symname
-OBJS = gkos.o chorder.o
+BINS = gkos symname chorder_test
+OBJS = gkos.o chorder.o chorder_test.o
 
 CFLAGS = -g -Wall -Wextra -Werror -Wno-error=unused-parameter -Wno-error=unused-function
 LDFLAGS = -g
@@ -10,8 +10,11 @@ clean:
 	$(RM) $(BINS) $(OBJS)
 
 gkos: gkos.o chorder.o -lX11 -lXi
-
 gkos.o: gkos.h
+
+chorder_test: chorder_test.o chorder.o
+chorder_test.o: chorder.h
+
 chorder.o: chorder.h
 
 symname: -lX11
---
+++ chorder_test.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <assert.h>
+
+#include "chorder.h"
+
+int mypress(void *unused, unsigned long code, int press)
+{
+	printf("%c %s\n", (char) code, press ? "pressed" : "released");
+	return 0;
+}
+
+int main()
+{
+	int rv;
+	struct chord_entry map[1][26];
+	int i;
+	map[0][0] = (struct chord_entry) {.type = TYPE_NONE, .val = 0};
+	for (i = 1; i < 26; i++)
+		map[0][i] = (struct chord_entry) {.type = TYPE_KEY, .val = '@' + i};
+	map[0][5].type = TYPE_MOD;
+
+	struct chorder kbd;
+	rv = chorder_init(&kbd, (struct chord_entry **) map, 1, 26, mypress, NULL);
+	assert(!rv);
+	rv = chorder_press(&kbd, 4);
+	rv = chorder_press(&kbd, 5);
+	rv = chorder_press(&kbd, 22);
+	rv = chorder_press(&kbd, 9);
+	rv = chorder_press(&kbd, 14);
+	chorder_destroy(&kbd);
+	return 0;
+}
