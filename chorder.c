f923f3598d7e30e7e859506ce973dd567e4236a0
Devin J. Pohly <djpohly@gmail.com>
2014-11-05 12:54:08 -0700

    add mod_stack manipulation functions

--- chorder.c
+++ chorder.c
@@ -5,6 +5,66 @@
 #include "chorder.h"
 
 /*
+ * Pushes a modifier onto a mod stack
+ */
+static int pushmod(struct mod_stack **mods, unsigned long code)
+{
+	struct mod_stack *tos = malloc(sizeof(*tos));
+	if (!tos)
+		return 1;
+
+	tos->code = code;
+	tos->next = *mods;
+	*mods = tos;
+	return 0;
+}
+
+/*
+ * Pops the top modifier off a mod stack
+ */
+static unsigned long popmod(struct mod_stack **mods)
+{
+	if (!*mods)
+		return 0;
+
+	struct mod_stack *tos = *mods;
+	unsigned long code = tos->code;
+	*mods = tos->next;
+	free(tos);
+	return code;
+}
+
+/*
+ * Determines whether a modifier is on a mod stack
+ */
+static int hasmod(struct mod_stack *mods, unsigned long code)
+{
+	for (/* mods */; mods; mods = mods->next)
+		if (mods->code == code)
+			return 1;
+	return 0;
+}
+
+/*
+ * Removes the given modifier from a mod stack, returning 0 if successful and
+ * nonzero otherwise
+ */
+static int removemod(struct mod_stack **mods, unsigned long code)
+{
+	struct mod_stack *node = *mods;
+	while (node) {
+		if (node->code == code) {
+			*mods = node->next;
+			free(node);
+			return 0;
+		}
+		mods = &(*mods)->next;
+		node = *mods;
+	}
+	return 1;
+}
+
+/*
  * Initializes a chorder
  */
 int chorder_init(struct chorder *kbd, struct chord_entry **entries, int maps,
